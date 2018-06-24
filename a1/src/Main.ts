cd7e25ef292ad2cfd48929b93c647887c9764eea
yunhui-hu <yunhui.hu@hpe.com>
2018-06-24 10:27:33 +0800

    add webchat game demo

---
+++ a1/src/Main.ts
@@ -0,0 +1,297 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-present, Egret Technology.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+class Main extends eui.UILayer {
+
+
+    protected createChildren(): void {
+        super.createChildren();
+
+        egret.lifecycle.addLifecycleListener((context) => {
+            // custom lifecycle plugin
+        })
+
+        egret.lifecycle.onPause = () => {
+            egret.ticker.pause();
+        }
+
+        egret.lifecycle.onResume = () => {
+            egret.ticker.resume();
+        }
+
+        //inject the custom material parser
+        //注入自定义的素材解析器
+        let assetAdapter = new AssetAdapter();
+        egret.registerImplementation("eui.IAssetAdapter", assetAdapter);
+        egret.registerImplementation("eui.IThemeAdapter", new ThemeAdapter());
+
+
+        this.runGame().catch(e => {
+            console.log(e);
+        })
+    }
+
+    private async runGame() {
+        await this.loadResource()
+        this.createGameScene();
+        const result = await RES.getResAsync("description_json")
+        this.startAnimation(result);
+        await platform.login();
+        const userInfo = await platform.getUserInfo();
+        console.log(userInfo);
+
+    }
+
+    private async loadResource() {
+        try {
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
+            await RES.loadConfig("resource/default.res.json", "resource/");
+            await this.loadTheme();
+            await RES.loadGroup("preload", 0, loadingView);
+            this.stage.removeChild(loadingView);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    private loadTheme() {
+        return new Promise((resolve, reject) => {
+            // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+            //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+            let theme = new eui.Theme("resource/default.thm.json", this.stage);
+            theme.addEventListener(eui.UIEvent.COMPLETE, () => {
+                resolve();
+            }, this);
+
+        })
+    }
+
+    private textfield: egret.TextField;
+    /**
+     * 创建场景界面
+     * Create scene interface
+     */
+    protected createGameScene(): void {
+        let sky = this.createBitmapByName("bg_jpg");
+        this.addChild(sky);
+        let stageW = this.stage.stageWidth;
+        let stageH = this.stage.stageHeight;
+        sky.width = stageW;
+        sky.height = stageH;
+
+        let topMask = new egret.Shape();
+        topMask.graphics.beginFill(0x000000, 0.5);
+        topMask.graphics.drawRect(0, 0, stageW, 172);
+        topMask.graphics.endFill();
+        topMask.y = 33;
+        this.addChild(topMask);
+
+        let icon: egret.Bitmap = this.createBitmapByName("egret_icon_png");
+        this.addChild(icon);
+        icon.x = 26;
+        icon.y = 33;
+
+        let line = new egret.Shape();
+        line.graphics.lineStyle(2, 0xffffff);
+        line.graphics.moveTo(0, 0);
+        line.graphics.lineTo(0, 117);
+        line.graphics.endFill();
+        line.x = 172;
+        line.y = 61;
+        this.addChild(line);
+
+
+        let colorLabel = new egret.TextField();
+        colorLabel.textColor = 0xffffff;
+        colorLabel.width = stageW - 172;
+        colorLabel.textAlign = "center";
+        colorLabel.text = "Hello Egret";
+        colorLabel.size = 24;
+        colorLabel.x = 172;
+        colorLabel.y = 80;
+        this.addChild(colorLabel);
+
+        let textfield = new egret.TextField();
+        this.addChild(textfield);
+        textfield.alpha = 0;
+        textfield.width = stageW - 172;
+        textfield.textAlign = egret.HorizontalAlign.CENTER;
+        textfield.size = 24;
+        textfield.textColor = 0xffffff;
+        textfield.x = 172;
+        textfield.y = 135;
+        this.textfield = textfield;
+
+        let button = new eui.Button();
+        button.label = "Click!";
+        button.horizontalCenter = 0;
+        button.verticalCenter = 0;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+
+        //add test code 20180617
+         var _myGrid:MyGrid = new MyGrid();
+        this.addChild( _myGrid );
+
+        //设定2个偏移量
+        var offsetX: number;
+        var offsetY: number;
+        //画一个红色的圆
+        var circle: egret.Shape = new egret.Shape();
+        circle.graphics.beginFill(0xff0000);
+        circle.graphics.drawCircle(25, 25, 25);
+        circle.graphics.endFill();
+        this.addChild(circle);
+        //手指按到屏幕，触发 startMove 方法
+        circle.touchEnabled = true;
+        circle.addEventListener(egret.TouchEvent.TOUCH_BEGIN, startMove, this);
+        //手指离开屏幕，触发 stopMove 方法
+        circle.addEventListener(egret.TouchEvent.TOUCH_END, stopMove, this);
+        function startMove(e: egret.TouchEvent): void {
+            //计算手指和圆形的距离
+            offsetX = e.stageX - circle.x;
+            offsetY = e.stageY - circle.y;
+            //手指在屏幕上移动，会触发 onMove 方法
+            this.stage.addEventListener(egret.TouchEvent.TOUCH_MOVE, onMove, this);
+            icon.scaleX = 2;
+            icon.scaleY = 2;
+        }
+        function stopMove(e: egret.TouchEvent) {
+            console.log(22);
+            //手指离开屏幕，移除手指移动的监听
+            this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, onMove, this);
+
+            icon.scaleX = 1;
+            icon.scaleY = 1;
+        }
+        function onMove(e: egret.TouchEvent): void {
+            //通过计算手指在屏幕上的位置，计算当前对象的坐标，达到跟随手指移动的效果
+            circle.x = e.stageX - offsetX;
+            circle.y = e.stageY - offsetY;
+
+            sprcon.swapChildren( spr11, spr22 );
+        }
+
+
+
+
+        var sprcon: egret.Sprite = new egret.Sprite();
+        this.addChild(sprcon);
+        sprcon.x = 10;
+        var spr11: egret.Sprite = new egret.Sprite();
+        spr11.graphics.beginFill(0xff0000);
+        spr11.graphics.drawRect(0, 0, 100, 100);
+        spr11.graphics.endFill();
+        spr11.x = 150;
+        spr11.y = 150;
+        sprcon.addChild(spr11);
+        var spr22: egret.Sprite = new egret.Sprite();
+        spr22.graphics.beginFill(0x00ff00);
+        spr22.graphics.drawRect(0, 0, 100, 100);
+        spr22.graphics.endFill();
+        spr22.x = 200;
+        spr22.y = 150;
+        sprcon.addChild(spr22);
+        var _spr:egret.DisplayObject = sprcon.getChildAt( 1 );
+        _spr.alpha = 0.5;
+
+
+        var shp:egret.Shape = new egret.Shape();
+        shp.graphics.lineStyle( 2, 0x00ff00 );
+        shp.graphics.moveTo( 68, 84 );
+        shp.graphics.lineTo( 167, 76 );
+        shp.graphics.lineTo( 221, 118 );
+        shp.graphics.lineTo( 290, 162 );
+        shp.graphics.lineTo( 297, 228 );
+        shp.graphics.lineTo( 412, 250 );
+        shp.graphics.lineTo( 443, 174 );
+        shp.graphics.endFill();
+        this.addChild( shp );
+    }
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name: string): egret.Bitmap {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    private startAnimation(result: Array<any>): void {
+        let parser = new egret.HtmlTextParser();
+
+        let textflowArr = result.map(text => parser.parse(text));
+        let textfield = this.textfield;
+        let count = -1;
+        let change = () => {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            let textFlow = textflowArr[count];
+
+            // 切换描述内容
+            // Switch to described content
+            textfield.textFlow = textFlow;
+            let tw = egret.Tween.get(textfield);
+            tw.to({ "alpha": 1 }, 200);
+            tw.wait(2000);
+            tw.to({ "alpha": 0 }, 200);
+            tw.call(change, this);
+        };
+
+        change();
+    }
+
+    /**
+     * 点击按钮
+     * Click the button
+     */
+    private onButtonClick(e: egret.TouchEvent) {
+        let panel = new eui.Panel();
+        panel.title = "Title";
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        panel.height = 300;
+        var listGroup = new components.ListGroup();
+        listGroup.width = 300;
+        listGroup.height = 100;
+        listGroup.x = 100;//panel.width/2 - listGroup.width/2;
+        listGroup.y = 50;//0;50;
+        panel.addChild(listGroup);
+        this.addChild(panel);
+    }
+}
